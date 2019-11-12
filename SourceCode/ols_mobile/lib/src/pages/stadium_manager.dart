246f09cf2aa69a2169e4b7271134b0725afbca57
anhlt <anhlt@oneempower.com>
2019-11-12 17:34:49 +0700

    no message

--- SourceCode/ols_mobile/lib/src/pages/stadium_manager.dart
+++ SourceCode/ols_mobile/lib/src/pages/stadium_manager.dart
@@ -67,10 +67,6 @@ class StadiumManagerState extends State<StadiumManager> {
                           _timeWidget("7h"),
                           _timeWidget("9h"),
                           _timeWidget("11h"),
-                          Container(
-                            height: ScreenUtil().setSp(30),
-                            color: Colors.grey,
-                          ),
                           _timeWidget("13h"),
                           _timeWidget("15h"),
                           _timeWidget("17h"),
@@ -79,30 +75,75 @@ class StadiumManagerState extends State<StadiumManager> {
                         ],
                       ),
                     ),
-                 Container(
-                   decoration: BoxDecoration(
-                     color: Colors.white,
-                   ),
-                       width: ScreenUtil().setSp(300),
-                       height: ScreenUtil().setSp(500),
-                       child: ListView.builder(
-                         scrollDirection: Axis.horizontal,
-                         itemCount: 6,
-                         itemBuilder: (context,index){
-                           return Column(
-                             children: <Widget>[
-                               _buildItemStadium(index + 1),
-                              //  Expanded(
-                              //    child: ListView.builder(
-                              //      itemCount: 6,
-                              //      itemBuilder: (context,index){
-                              //        return 
-                              //      },
-                              //    ),
-                              //  )
-                             ],
-                           );
-                         },
+                 Expanded(
+                  //  decoration: BoxDecoration(
+                  //    color: Colors.white,
+                  //  ),
+                  //      width: ScreenUtil().setSp(300),
+                  //      height: ScreenUtil().setSp(500),
+                       child: Container(
+                          height: ScreenUtil().setSp(500),
+                         child: ListView.builder(
+                           scrollDirection: Axis.horizontal,
+                           itemCount: 6,
+                           itemBuilder: (context,index){
+                             return Column(
+                               children: <Widget>[
+                                 _buildItemStadium(index + 1),
+                                  Expanded(
+                                    child: Container(
+                                      width: ScreenUtil().setSp(50),
+                                      height: ScreenUtil().setSp(600),
+                                      child: ListView.builder(
+                                         itemCount: 7,
+                                         itemBuilder: (context,index){
+                                           return Container(
+                                             decoration: BoxDecoration(
+                                              color: Colors.white,
+                                              border: Border(
+                                                left:  BorderSide(
+                                                  color: Colors.green
+                                                ),
+                                                top:  index == 0 ? BorderSide(
+                                                  color: Colors.green
+                                                ) : BorderSide(
+                                                  width: 0,
+                                                ),
+                                                  bottom: BorderSide(
+                                                  color: Colors.green
+                                                ),
+                                                right:  index == 6 ? BorderSide(
+                                                  color: Colors.green
+                                                ) : BorderSide(
+                                                  width: 0,
+                                                ),
+                                              )
+                                             ),
+                                             width: ScreenUtil().setSp(50),
+                                              height: ScreenUtil().setSp(40),
+                                              child: index != 2 ? Center(
+                                                child: Container(
+                                                  width: ScreenUtil().setSp(25),
+                                                  height: ScreenUtil().setSp(25),
+                                                  decoration: BoxDecoration(
+                                                  color: Colors.green,
+                                                  borderRadius: BorderRadius.circular(100)
+                                                  ),
+                                                ),
+                                              ) : Container(
+                                                 width: ScreenUtil().setSp(50),
+                                              height: ScreenUtil().setSp(40),
+                                              color: Colors.grey,
+                                              ),
+                                           );
+                                         },
+                                       ),
+                                    ),
+                                  ),
+                               ],
+                             );
+                           },
+                         ),
                        ),
                      )
                     
@@ -123,8 +164,8 @@ class StadiumManagerState extends State<StadiumManager> {
 
   _buildItemStadium(int index){
     return Container(
-             width: ScreenUtil().setSp(60),
-            height: ScreenUtil().setSp(60),
+             width: ScreenUtil().setSp(50),
+            height: ScreenUtil().setSp(50),
             padding: EdgeInsets.all(17),
       child: Container(
             width: ScreenUtil().setSp(25),
